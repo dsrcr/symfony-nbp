705bbede468f2eddac499098f386ef33984b8df3
dsorcererdev <dsorcererdev@yahoo.com>
2022-07-06 14:35:43 +0200

    Get currency from api and add it to database

--- src/Controller/CurrencyController.php
+++ src/Controller/CurrencyController.php
@@ -16,10 +16,24 @@ use Symfony\Contracts\HttpClient\HttpClientInterface;
 class CurrencyController extends AbstractController
 {
     #[Route('/addnewcurrency', name: 'app_currency')]
-    public function addNewCurrency(HttpClientInterface $httpClient): array
+    public function addNewCurrency(HttpClientInterface $httpClient, ManagerRegistry $doctrine): Response
     {
         $response = $httpClient->request('GET', 'http://api.nbp.pl/api/exchangerates/tables/A?format=json');
         $data = $response->toArray();
-        dd($data[0]['rates']);
+        $currencies = $data[0]['rates'];
+        $currency = $currencies[1];
+
+        $name = strval($currency['currency']);
+        $currency_code = strval($currency['code']);
+        $exchange_rate = strval($currency['mid']);
+
+        $entityManager = $doctrine->getManager();
+            $currency = new Currency();
+            $currency->setName($name);
+            $currency->setCurrencyCode($currency_code);
+            $currency->setExchangeRate($exchange_rate);
+            $entityManager->persist($currency);
+            $entityManager->flush();
+        return new Response('The currencies has been added'); 
     }
 }
