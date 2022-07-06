c63cac582b2b0215cdfe8c5d34be0b9b61a087d4
dsorcererdev <dsorcererdev@yahoo.com>
2022-07-06 12:54:11 +0200

    Add Currency service, test the CurrencyController

--- src/Controller/CurrencyController.php
+++ src/Controller/CurrencyController.php
@@ -2,9 +2,14 @@
 
 namespace App\Controller;
 
+use App\Entity\Currency;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
+
+use App\Entity\Product;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\HttpFoundation\Response;
 
 class CurrencyController extends AbstractController
 {
@@ -16,4 +21,22 @@ class CurrencyController extends AbstractController
             'path' => 'src/Controller/CurrencyController.php',
         ]);
     }
+
+    #[Route('/addnewcurrency', name: 'app_currency')]
+    public function addNewCurrency(ManagerRegistry $doctrine): Response
+    {
+
+        $entityManager = $doctrine->getManager();
+
+        $product = new Currency();
+        $product->setName('bat (Tajlandia)');
+        $product->setCurrencyCode('THB');
+        $product->setExchangeRate(0.1298);
+        $entityManager->persist($product);
+        $entityManager->flush();
+
+        return new Response('Saved new currency with id ' . $product->getId());
+    }
+
+
 }
